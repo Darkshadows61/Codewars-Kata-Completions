78abaccd785041d33afebc65684797a255c2917f
Thomas Bisom <bisomt@gmail.com>
2022-03-25 17:34:53 -0400

    3.25.22.1
    second update today

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -21,4 +21,13 @@ return Number(bac)
 }
 //test: bloodAlcoholContent({ounces:12.5, abv:0.4}, 190, 'female', 1) //deep.Equal 0.0743
 
+//8kyu Total Pressure Calculation
+
+solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+    let pTotal = ((((givenMass1/molarMass1) + (givenMass2/molarMass2)) * ((0.082 * (temp + 273.15))) / volume))
+    return pTotal
+   }
+   
+//test solution(44, 30, 3, 2, 5, 50)  deep.Equal=0.7146511212121212
+
 //8kyu 
