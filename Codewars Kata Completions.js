56b36a6f6f04de8dd743f3e421a100670b346cfd
Darkshadows61 <bisomt@gmail.com>
2022-03-27 18:12:51 -0400

    3.27.22
    count sheep 8kyu

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -38,4 +38,12 @@ function dBScale(intensity) {
 
 //test (Math.round(dBScale(Math.pow(10, -11))))
 
+//8kyu Count Sheep in sleep
+const countSheep = num => {
+    let str = "";
+    for (let i = 1; i <= num; i++) {str+=`${i} sheep...`}
+    return str
+}
+//test console.log(countSheep(5))
+
 //8kyu 
