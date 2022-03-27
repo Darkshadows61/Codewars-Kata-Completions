7af0caca5dc7ccd60efe359c2d9fbc591f1ed7ab
Darkshadows61 <bisomt@gmail.com>
2022-03-27 18:22:26 -0400

    3.27.22 pt2
    8kyu convert string to array

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -1,13 +1,13 @@
 //Codewars Kata Completions
+//Tom Bisom @Darkshadows61
 
-
-//8kyu Mulitply
+//8kyu Mulitply 3.24.22
 function multiply(a, b){
     return a * b
 }
 //test: multiply(3,4) deep.Equal 12
 
-//8kyu Blood-Alcohol Content Calculator
+//8kyu Blood-Alcohol Content Calculator 3.24.22
 //Create a BAC calc that takes in the following parametes, adjusts exquation based on m or f, and uses the drinks obj. 
 function bloodAlcoholContent(drinks, weight, sex, time){
     drinks = (drinks.ounces * drinks.abv);
@@ -21,8 +21,7 @@ return Number(bac)
 }
 //test: bloodAlcoholContent({ounces:12.5, abv:0.4}, 190, 'female', 1) //deep.Equal 0.0743
 
-//8kyu Total Pressure Calculation
-
+//8kyu Total Pressure Calculation 3.25.22
 solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
     let pTotal = ((((givenMass1/molarMass1) + (givenMass2/molarMass2)) * ((0.082 * (temp + 273.15))) / volume))
     return pTotal
@@ -30,7 +29,7 @@ solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
    
 //test solution(44, 30, 3, 2, 5, 50)  deep.Equal=0.7146511212121212
 
-//8kyu decibel Scale
+//8kyu decibel Scale 3.26.22
 function dBScale(intensity) {
     let dB = 10 * Math.log10(intensity / (10 ** -12))
     return dB
@@ -38,12 +37,20 @@ function dBScale(intensity) {
 
 //test (Math.round(dBScale(Math.pow(10, -11))))
 
-//8kyu Count Sheep in sleep
+//8kyu Count Sheep in sleep 3.27.22
 const countSheep = num => {
     let str = "";
     for (let i = 1; i <= num; i++) {str+=`${i} sheep...`}
     return str
 }
 //test console.log(countSheep(5))
+
+//8kyu Convert a string to an Array 3.27.22
+stringToArray = string => {
+    const text = (string)
+    newArr = text.split(" ");
+    return newArr
+}
+//test console.log(stringToArray("robin singh"))
 
 //8kyu 
