8eac0895e3eca2c5370bffeb7abf4827fb301b10
Darkshadows61 <bisomt@gmail.com>
2022-03-28 04:14:51 -0400

    3.28.22 update 1
    First 7kyu Digit Explosion

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -73,3 +73,10 @@ const flip = (d,a) => {
     }
 }
 //test flip('R', [3,2,1,2])
+
+//7kyu Digits Explosion repeat a value a number of times equal to the value ex. 3 = 333, 7, = 7777777
+const explode = s => {
+    let arr2 = s.toString().split('').map((e) => e.repeat(+e)).join("")
+    return arr2
+    }
+//test explode(31245)
