8823cec0a61bf60c52f11cc7144cf403dd562d14
Darkshadows61 <bisomt@gmail.com>
2022-03-30 11:51:46 -0400

    3.30.22 update 1
    8kyu Array.map Sum

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -120,4 +120,16 @@ lovefunc = (flower1, flower2) => {
   }
 //test lovefunc(1,4) then lovefunc(0,0)
 
-//7kyu 
+//8kyu Total Points
+function points(games) {
+    let total = 0;
+    games.map(game => {
+      if (game[0] === game[2]) {
+        total += 1;
+      } else if (game[0] > game[2]) {
+        total += 3;
+      }
+    });
+    return total;
+  }
+points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3'])
