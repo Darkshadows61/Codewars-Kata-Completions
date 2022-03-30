3fe0ab57f7a239f722409a7474b5a5230e7b8ecb
Darkshadows61 <bisomt@gmail.com>
2022-03-30 16:42:32 -0400

    3.30.22 update 2
    8kyu Repeat String (gg ez)

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -132,4 +132,10 @@ function points(games) {
     });
     return total;
   }
-points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3'])
+//test points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3'])
+
+//8kyu String Repeat
+repeatStr = (n,s) => {
+  return n.repeat(s)
+}
+//test repeatStr('X', 10)
