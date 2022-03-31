c7d99c9715238264c5c63d7480b9e13516a133b3
Darkshadows61 <bisomt@gmail.com>
2022-03-31 11:25:34 -0400

    3.31.22 update 1
    7kyu Nice Array

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -149,4 +149,18 @@ function arrayPlusArray(arr1, arr2) {
   }
 //test arrayPlusArray([1, 2, 3], [4, 5, 6]), 21)
 
-//8kyu 
+//7kyu Nice Array: if the values in the array are n-1 or n+1, is nice
+const isNice = (arr) => {
+  if (arr.length <= 0) {
+    return false
+  }
+  const result = arr.every(item => {
+    return arr.some(current => {
+      return item === current+1 || item === current -1;
+    });
+  });
+  
+  console.log("Result:", result);
+  return result
+}
+//test inNice([2,10,9,3], true)
