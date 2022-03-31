a7a587234d25ff5d399fec752c60ecf81f2bcd9a
Darkshadows61 <bisomt@gmail.com>
2022-03-30 23:00:27 -0400

    3.30.22 update 3
    8kyu array concat and sum

--- Codewars Kata Completions.js
+++ Codewars Kata Completions.js
@@ -139,3 +139,14 @@ repeatStr = (n,s) => {
   return n.repeat(s)
 }
 //test repeatStr('X', 10)
+
+//8kyu Array Concat and Sum
+function arrayPlusArray(arr1, arr2) {
+  let newArr =  arr1.concat(arr2)
+  const initial = 0;
+  const sum = newArr.reduce((a,t) => a + t,initial)
+  return sum
+  }
+//test arrayPlusArray([1, 2, 3], [4, 5, 6]), 21)
+
+//8kyu 
