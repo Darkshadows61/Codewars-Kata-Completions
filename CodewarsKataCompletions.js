dec92d06a0339a457aefe109332ed0fc7ff95da9
Darkshadows61 <bisomt@gmail.com>
2022-04-01 12:57:13 -0400

    4.1.22 update 1
    8yu first non-consecutive number

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -164,3 +164,14 @@ const isNice = (arr) => {
   return result
 }
 //test inNice([2,10,9,3], true)
+
+//8kyu Find the first non-consecutive number
+firstNonConsecutive = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    const num = arr[i] + 1
+    const consecutive = arr[i + 1]
+    if (num !== consecutive) return consecutive
+    }
+  return null
+  }
+firstNonConsecutive([1,2,3,4,6,7,8])
