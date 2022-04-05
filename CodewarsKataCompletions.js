832d7f3115bea36ffd18ade485fe519a87ddf48e
Thomas Bisom <bisomt@gmail.com>
2022-04-05 10:56:20 -0400

    4.5.22 update 1
    8kyu Lario and Muigi Kata

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -246,6 +246,13 @@ const rps = (p1, p2) => {
     }
   };
 
+//8kyu Lario and Muigi Pipes; Add 1 to an array value then concatenate. The acutal Kata wanted more than this.
+function pipeFix(numbers){
+  let newNumbers = numbers.map(x => x + 1)
+  return numbers.concat(newNumbers).sort()
+}
+//test pipeFix([1,3,5,7])
+
 //eloquent JS Chap 4 Example 1 Sum of an Array
 range = (start, end) => {
   let sum = start;
