7fd384518a08f565a2273c7f2126290c190d9cc7
Thomas Bisom <bisomt@gmail.com>
2022-04-04 22:28:01 -0400

    4.4.22 update 1
    8kyu rock paper scissors

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -231,6 +231,21 @@ countBs = (b) => {
   return(bs)
 }
 
+//8kyu Rock Paper Scissors!
+const rps = (p1, p2) => {
+  if (p1 === 'rock' && p2 === 'paper') {
+    return "Player 2 won!" }
+    else if (p1 === 'paper' && p2 === 'scissors') {
+      return "Player 2 won!"
+    } else if (p1 === 'scissors' && p2 === 'rock') {
+      return "Player 2 won!"
+    } else if (p1 === p2) {
+      return "Draw!"
+    } else {
+      return "Player 1 won!"
+    }
+  };
+
 //eloquent JS Chap 4 Example 1 Sum of an Array
 range = (start, end) => {
   let sum = start;
