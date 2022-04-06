fef30baedfb9104a802a1217010c2b7da1f04e30
Thomas Bisom <bisomt@gmail.com>
2022-04-06 16:30:33 -0400

    4.4.6.22 update 2
    6kyu count the values in a string
    impressed myself with this one

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -275,3 +275,36 @@ return arr.reverse()
 //test revArr([1,2,3,4,5])
 
 //eloquent JS Chap 4 example 3 A List
+arrayToList = (arr) => {
+  let list = {
+    value: arr[0],
+    rest: {
+      value: arr[1],
+      rest: {
+        value: arr[2],
+        rest: null
+      }
+    }
+  }
+  return list
+}
+
+//6kyu count values in a string
+function count (string) { 
+    let as = 0;
+    let bs = 0;
+    for (i = 0; i <= string.length; i++) {
+      if (string.charAt(i) === 'a') {
+        as++;
+      } else if (string.charAt(i) === 'b') {
+        bs++
+      } else if (string === '') {
+        return {}
+      }
+    }
+    newArr = (string.split(''));
+    return `${newArr[0]}: ${as}, ${newArr[1]}: ${bs}`
+  }
+//test count('aba')
+
+//
