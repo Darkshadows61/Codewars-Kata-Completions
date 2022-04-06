b1e2b8177ad0550e978a57f344fdc9ff44070875
Thomas Bisom <bisomt@gmail.com>
2022-04-06 16:54:09 -0400

    4.6.22 update 3
    eloquent JS Chap 4 Ex 4 Deep Equal
    ::first tryyyy::

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -307,4 +307,33 @@ function count (string) {
   }
 //test count('aba')
 
-//
+//eloquent JS Chap 4 Example 4 Deep Comparison
+const obj1 = {
+a: 1,
+b: 'got item',
+}
+const obj2 = {
+  a: 1,
+  b: 'got item',
+}
+const obj3 = {
+  a: 2,
+  b: 'got item',
+}
+const obj4 = {
+  a: 1,
+  b: 'got items'
+}
+deepEqual = (x,y) => {
+  if (x.a === y.a) {
+return "True"
+  } else if (x.a != y.a) {
+    return "False"
+  } else if (x.b === y.b) {
+    return "True"
+  } else if (x.b != y.b) {
+    return "False"
+  } else {
+    return null
+  }
+}
