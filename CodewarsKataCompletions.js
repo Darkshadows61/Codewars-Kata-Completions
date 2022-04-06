d5371d64d59b4b7c36aaa014a718a07c7f88b1b7
Thomas Bisom <bisomt@gmail.com>
2022-04-06 08:47:47 -0400

    4.6.22 update 1
    eloquent JS chap 4 example 2

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -255,10 +255,23 @@ function pipeFix(numbers){
 
 //eloquent JS Chap 4 Example 1 Sum of an Array
 range = (start, end) => {
-  let sum = start;
-    for (i = start; i <= end; i++) {
-      return sum++
+  let newArr = [];
+  count = start;
+  for (i = 0; i <= end; i++) {
+    if (i < ((end+1)-start)) {
+      newArr.push(count++)
     }
-    
+  }
+    let sum = newArr.reduce(
+    (previous, current) => previous + current);
+    return sum
 }
-//test 
+//test range(1,10) should = 55
+
+//eloquent JS Chap 4 Example 2 Reversing an Array
+revArr = (arr) => {
+return arr.reverse()
+}
+//test revArr([1,2,3,4,5])
+
+//eloquent JS Chap 4 example 3 A List
