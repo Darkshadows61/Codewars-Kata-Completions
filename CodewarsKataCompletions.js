0f31c76c2cbde951c36339a0d4da017bf999cfad
Thomas Bisom <bisomt@gmail.com>
2022-04-08 11:19:54 -0400

    4.8.22 update 1
    7kyu find the middle characters

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -349,3 +349,15 @@ function descendingOrder(n){
 disemvowel = (str) => {
  return str.replace(/A|E|I|O|U|a|e|i|o|u/g, '') 
 }
+
+//7kyu find the middle character
+function getMiddle(s) {
+  const even = (s.length % 2 === 0)
+  if (even === true) {
+    one = s.charAt((s.length / 2) -1)
+    two = s.charAt(s.length / 2)
+    return one + two
+  } else {
+    return s.charAt(s.length/2)
+  }
+}
