19f0ad4e8146a7d310f14f40ac63c0b258699e06
Thomas Bisom <bisomt@gmail.com>
2022-04-09 12:21:20 -0400

    4.9.22 update 1
    7kyu kata find the shortest word and return the length as an integer

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -361,3 +361,13 @@ function getMiddle(s) {
     return s.charAt(s.length/2)
   }
 }
+
+//7kyu shortest word
+function findShort(s) {
+  let word = s.split(" ")
+  let min = word.map(word => word.length).sort((a,b) => a-b)
+  return min[0]
+}
+//test findShort('bitcoin may small after all) returns 3
+
+//
