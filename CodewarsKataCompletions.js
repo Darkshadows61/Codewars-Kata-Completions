9dccc06bfca2f5c73d94ebc0f020295defbe1f93
Darkshadows61 <bisomt@gmail.com>
2022-04-11 15:15:33 -0400

    4.11.22 update 4
    7kyu DropCaps

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -411,8 +411,14 @@ function longest(words) {
 
 //7kyu Dropcaps
 function dropCap(n) {
-let drop = n.split(' ')
-drop.map(x => n.charAt([0]).toUpperCase)
-return drop
+  n = n.split(' ');
+  for(var i = 0; i < n.length; i++) {
+    if(n[i].length > 2) {
+      n[i] = n[i].slice(0, 1).toUpperCase() + n[i].slice(1).toLowerCase();
+    }
+  }
+  return n.join(' ');
 }
 //test dropCap('Revelation of the contents outraged American public opinion, and helped generate')
+
+//
