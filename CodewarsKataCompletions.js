5237a401ce3f0156dbbae29d9b3d360bdcc7390e
Darkshadows61 <bisomt@gmail.com>
2022-04-12 17:15:44 -0400

    4.12.22 update 1
    7kyu Credit Card Mask

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -421,4 +421,11 @@ function dropCap(n) {
 }
 //test dropCap('Revelation of the contents outraged American public opinion, and helped generate')
 
-//
+//Credit Card Mask hide all the digits with # except for the last 4
+function maskify(cc) {
+  let end = cc.slice(-4)
+  let count = cc.slice(0,-4).length
+  mask = '#'
+  return `${mask.repeat(count)}${end}`
+}
+//test maskify('4556364607935616')
