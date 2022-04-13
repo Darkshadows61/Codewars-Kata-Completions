160ff869c2586d3741971958d2f94c814ac910d1
Darkshadows61 <bisomt@gmail.com>
2022-04-13 09:16:40 -0400

    4.13.22 update 1
    7kyu Printer Errors

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -429,3 +429,12 @@ function maskify(cc) {
   return `${mask.repeat(count)}${end}`
 }
 //test maskify('4556364607935616')
+
+//7kyu printer error
+function printerError(s) {
+  let length = s.length;
+  const regex = /[n-z]/g;
+  let error = Number(s.search(regex))
+  return `${length-error}/${length}`
+}
+//test printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz") should equal "3/56"
