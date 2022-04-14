67d52d0923fbde7d797bcd48a203af1387a540b9
Darkshadows61 <bisomt@gmail.com>
2022-04-14 14:58:50 -0400

    4.14.22 update 1
    7kyu unused number filter

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -438,3 +438,13 @@ function printerError(s) {
   return `${length-error}/${length}`
 }
 //test printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz") should equal "3/56"
+
+//7kyu Filter Unused Digits
+const unusedDigits = (...arr) => {
+  var digits = arr.join();
+
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    .filter(x => !digits.includes(x))
+    .join('');
+};
+//test unusedDigits(12, 34, 56, 78)
