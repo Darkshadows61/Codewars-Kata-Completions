51825f441cb714629cb6779da7226437a34026a1
Darkshadows61 <bisomt@gmail.com>
2022-04-19 09:43:21 -0400

    4.19.22 update 1
    8kyu square or square root
    -feels like a 7kyu, but was fun.

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -496,4 +496,16 @@ function isSquare(n) {
 }
 //test isSquare(25), true
 
-//
+//8kyu to square or not to square
+function squareOrSquareRoot(array) {
+  let arr = array
+  for (i = 0; i < array.length; i++){
+    if (Number.isInteger(Math.sqrt(arr[i])) ){
+      arr[i] = Math.sqrt(arr[i])
+    } 
+    else {
+      arr[i] = arr[i] * arr[i] 
+    }
+  } return arr;  
+}
+//test squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ])
