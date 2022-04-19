3f79584aa560936159d7cc6087304b912e0293cc
Darkshadows61 <bisomt@gmail.com>
2022-04-19 18:21:24 -0400

    4.19.22update 2
    moar katas

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -511,11 +511,24 @@ function squareOrSquareRoot(array) {
 //test squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ])
 
 //7kyu barista
-function barista(coffees) {
-  order = coffees.sort((a,b) => (a-b))
-  timePer = order.map(x => x+2)
-    for (i=0; i<timePer.length; i++)
-    timePer[i] = timePer[i] + timePer[i]
-  return timePer
-
+function barista(coffees){
+  coffees.sort((a,b)=> a-b)
+  return coffees.reduce((acc, cookTime, i) => acc + cookTime * (coffees.length-i) + 2*i, 0)
 }
+//test barista([2,10,5,3,9])
+
+function tester () {
+let test = ['11','22','33','44','55','66','77','88','99','00']
+chance = test.map(t => t[0])
+return chance.join('')
+}
+
+
+//test for reduce
+const newNumbers = [1, 3, 5, 7];
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of the accumulator: ', accumulator)
+  console.log('The value of the currentValue: ', currentValue)
+  return accumulator + currentValue
+});
+
