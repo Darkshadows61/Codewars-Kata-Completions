95a33019411668ac4e1a925b8bfba246fe3792ea
Darkshadows61 <bisomt@gmail.com>
2022-04-21 11:10:53 -0400

    4.21.22 update 1
    8kyu litres and 7kyu Jaden Case

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -546,4 +546,16 @@ function filter_list(l) {
 
 //test filter_list([1,2,'aasf','1','123',123])
 
-//
+//8kyu Keep Hydrated
+function litres(time) {
+  return Math.floor(time/2)
+}
+//test litres(11.8) === 5
+
+//7kyu Jaden Casing
+String.prototype.toJadenCase = function () { 
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
+//test str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real")
