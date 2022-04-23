f6a33a496031570321041d3b84c38ab99e796124
Darkshadows61 <bisomt@gmail.com>
2022-04-23 12:09:10 -0400

    4.23.22 update 2
    6kyu who likes this?

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -580,4 +580,20 @@ function createPhoneNumber(numbers) {
 }
 //test createPhoneNumber([1,2,3,4,5,6,7,8,9,0]) === (123) 456-7890
 
+//6kyu Who likes this?
+function likes(names) {
+  if (names.length === 0) {
+    return 'no one likes this'
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } else if (names.length > 3) {
+    return `${names[0]}, ${names[1]} and ${names.length -2} others like this`
+  }
+}
+//test likes([jack, adam, marshall, rick]) === 'jack, adam and 2 others like this'
+
 //
