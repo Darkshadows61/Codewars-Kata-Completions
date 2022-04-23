fb41ee9555fc5f88756a4dbb599a0ff034aec57a
Darkshadows61 <bisomt@gmail.com>
2022-04-23 11:58:01 -0400

    4.23.22 update 1
    6kyu create phone number
    ggez

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -573,3 +573,11 @@ function getSum (a,b) {
   return (max - min + 1) * (min + max) / 2;
 }
 //test getSum(5,0) === 15
+
+//6kyu Create Phone Number
+function createPhoneNumber(numbers) {
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+//test createPhoneNumber([1,2,3,4,5,6,7,8,9,0]) === (123) 456-7890
+
+//
