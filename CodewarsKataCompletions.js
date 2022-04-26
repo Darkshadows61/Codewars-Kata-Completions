871b7d62350592a661d4563e2f809bdd55ba1166
Darkshadows61 <bisomt@gmail.com>
2022-04-26 12:33:38 -0400

    4.26.22update 1
    couple 8kyus to get going

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -629,4 +629,24 @@ function arrayDiff(a,b) {
 }
 //test arrayDiff([1,2,2], [2])
 
-//
+//8kyu Bin to Decimel
+function binToDec(bin) {
+  bin.split('').Number()
+  return ((bin[0]* (2**bin.length))+(bin[1]* (2**(bin.length-1)))+(bin[2]* (2**(bin.length-2)))+(bin[3]* (2**(bin.length -3)))+(bin[4]* (2**(bin.length-4)))+(bin[5]* (2**(bin.length-5)))+(bin[6]* (2**(bin.length-6)))+(bin[7]* (2**(bin.length-7))))
+}
+//test binToDec(1001001) === 73 in binary
+
+//8kyu circular obects
+class Circular {
+  constructor() {
+    this.value = "Hello World";
+    this.self = this;
+  }
+}
+
+circular = new Circular();
+
+//8kyu !a =a?!
+const a = []
+
+//8kyu 
