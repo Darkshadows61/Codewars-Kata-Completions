e1ddb622a74328e0008c053e894ed889970742de
Darkshadows61 <bisomt@gmail.com>
2022-04-27 16:04:18 -0400

    4.27.22 update 1
    quick 8kyu on basic math

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -649,4 +649,16 @@ circular = new Circular();
 //8kyu !a =a?!
 const a = []
 
-//8kyu 
+//8kyu Basic Mathematical Operations
+function basicOp(operation, value1, value2) {
+  if (operation === '+') {
+    return value1 + value2
+  } else if (operation === '-') {
+    return value1 - value2
+  } else if (operation === '*') {
+    return value1 * value2
+  } else if (operation === '/') {
+    return value1/value2
+  }
+}
+//test basicOp('+', 4, 7)
