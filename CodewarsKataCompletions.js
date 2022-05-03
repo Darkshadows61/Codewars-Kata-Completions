34b9b2e8cbd058f3596848a4b02ae64430f27382
Darkshadows61 <bisomt@gmail.com>
2022-05-03 13:28:33 -0400

    5.3.22 update 1
     8kyu isDivisible

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -727,4 +727,12 @@ function century(year) {
   }
 }
 
-//
+//8kyu isDivisible
+function isDivisible(n,x,y) {
+  if (n%x === 0 && n%y === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+//test isDivisible(3,3,4) === false
