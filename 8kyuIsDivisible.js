7aa1a439ae4f70c3a5c803353464467c41f87049
Darkshadows61 <bisomt@gmail.com>
2022-05-16 13:44:11 -0400

    1.1.22 update1

---
+++ 8kyuIsDivisible.js
@@ -0,0 +1,10 @@
+//8kyu isDivisible
+function isDivisible(n,x,y) {
+    if (n%x === 0 && n%y === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+  //test isDivisible(3,3,4) === false
+  
