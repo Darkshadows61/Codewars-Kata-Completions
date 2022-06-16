f95c760c3f1eac9f2428a8e952394c6fd8c42f47
Darkshadows61 <bisomt@gmail.com>
2022-06-16 14:29:35 -0400

    7kyuSumTwoSmallestNumbers

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -2,3 +2,13 @@
 //Tom Bisom @Darkshadows61
 //Space to test code before creating a seperate file with the finished product
 
+function sumTwoSmallestNumbers(numbers) {  
+    //sum of 2 lowest possible numbers
+    //sort the numbers so they are in order from low to high
+    numbers.sort((a,b) => a - b)
+    //then, take the first two in the array and add together
+    let result = numbers[0] + numbers[1]
+    return result;
+  }
+
+  //test sumTwoSmallestNumbers([5, 8, 12, 19, 22]) === 13
