5eb7cf4f088a366f5ca0a0c820ccee2bd02f11d8
Darkshadows61 <bisomt@gmail.com>
2022-06-18 13:32:31 -0400

    7kyuFriendOrFoe

---
+++ 7kyuFriendOrFoe.js
@@ -0,0 +1,5 @@
+function friend(friends){
+    return friends.filter(word => word.length == 4)
+}
+
+//test friend(["Ryan", "Kieran", "Mark"])
--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -2,12 +2,3 @@
 //Tom Bisom @Darkshadows61
 //Space to test code before creating a seperate file with the finished product
 
-function twoSum(nums, target) {
-    for (i=0; i<nums.length; i++) {
-        if (i + i == target) {
-            console.log('y')
-        }
-    }
-}
-
-//test twoSum([2,7,11,13], 9)
