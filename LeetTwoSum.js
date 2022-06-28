02dad3d63b46fc3dce9dd99ff032d5be18da17e9
Darkshadows61 <bisomt@gmail.com>
2022-06-28 04:19:14 -0400

    LeetTwoSums

--- LeetTwoSum.js
+++ LeetTwoSum.js
@@ -1,7 +1,3 @@
-//Codewars Kata Completions
-//Tom Bisom @Darkshadows61
-//Space to test code before creating a seperate file with the finished product
-
 function twoSum(nums, target) {
     for (i=0; i<nums.length; i++) {
         for (j=0; j<nums.length; j++) {
