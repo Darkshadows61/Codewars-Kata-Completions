b67f7aea0b348a2b6f547e4bf39d9b1c9995bf01
Darkshadows61 <bisomt@gmail.com>
2022-06-29 02:17:23 -0400

    LeetAddTwoNumbers

--- IndeedInterviewSortAccessTimes.js
+++ IndeedInterviewSortAccessTimes.js
@@ -1,7 +1,3 @@
-//Codewars Kata Completions
-//Tom Bisom @Darkshadows61
-//Space to test code before creating a seperate file with the finished product
-
 /*
 Suppose we have an unsorted log file of accesses to web resources. Each log entry consists of an access time, the ID of the user making the access, and the resource ID. 
 
@@ -70,48 +66,3 @@ Complexity analysis variables:
 
 n: number of logs in the input
 */
-
-"use strict";
-
-const logs1 = [
-    ["58523", "user_1", "resource_1"],
-    ["62314", "user_2", "resource_2"],
-    ["54001", "user_1", "resource_3"],
-    ["200", "user_6", "resource_5"],   
-    ["215", "user_6", "resource_4"],
-    ["54060", "user_2", "resource_3"],
-    ["53760", "user_3", "resource_3"],
-    ["58522", "user_22", "resource_1"],
-    ["53651", "user_5", "resource_3"],
-    ["2", "user_6", "resource_1"],
-    ["100", "user_6", "resource_6"],
-    ["400", "user_7", "resource_2"],
-    ["100", "user_8", "resource_6"],
-    [ "54359", "user_1", "resource_3"],
-];
-
-const logs2 = [
-    ["300", "user_1", "resource_3"],
-    ["599", "user_1", "resource_3"],
-    ["900", "user_1", "resource_3"],
-    ["1199", "user_1", "resource_3"],
-    ["1200", "user_1", "resource_3"],
-    ["1201", "user_1", "resource_3"],
-    ["1202", "user_1", "resource_3"],
-];
-
-const logs3 = [
-    ["300", "user_10", "resource_5"],
-];
-
-function sort(logs1) {
-    let newArr =[];
-    let i;
-    let j;
-    for(i=0;i<logs1.length; i++) {
-      newArr.push(logs1[i][1])
-    }
-    console.log(newArr)
-}
-
-sort(logs1)
