748d9a744b7aa4e68e914e2874e89aa8832bf683
Darkshadows61 <bisomt@gmail.com>
2022-08-03 19:01:12 -0400

    8kyu training on sum array

--- CodewarsKataCompletions.js
+++ CodewarsKataCompletions.js
@@ -2,3 +2,8 @@
 //Tom Bisom @Darkshadows61
 //Space to test code before creating a seperate file with the finished product
 
+// Sum Numbers
+function sum (numbers) {
+   return numbers.reduce((a,b)=> a + b, 0)
+};
+//test sum([1, 5.2, 4, 0, -1])
---
+++ TrainingOnSumArray.js
@@ -0,0 +1,4 @@
+function sum (numbers) {
+    return numbers.reduce((a,b)=> a + b, 0)
+ };
+//test sum([1, 5.2, 4, 0, -1]) == 9.2
