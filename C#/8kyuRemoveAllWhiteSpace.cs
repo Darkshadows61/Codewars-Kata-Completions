f57fa7b0d4c8c4ce907b392fc4b6df249dffb30e
Darkshadows61 <bisomt@gmail.com>
2022-08-22 02:38:29 -0400

    remove all white space

---
+++ C#/8kyuRemoveAllWhiteSpace.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+namespace Solution 
+{
+  public static class SpacesRemover
+  {
+    public static string NoSpace(string input)
+    {
+        return input.Replace(" ", "");
+    }  
+  }
+}
