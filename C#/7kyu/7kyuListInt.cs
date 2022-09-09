734f894f9e430bdc519804ee3c23aeababa33204
Darkshadows61 <bisomt@gmail.com>
2022-09-09 02:39:37 -0400

    new kata with lists

---
+++ C#/7kyu/7kyuListInt.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ListFilterer
+{
+   public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+   {
+      return listOfItems.OfType<int>(); 
+   }
+}
